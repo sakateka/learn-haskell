d26884f70f754fa7ba3cc39b76242253015e4ec5
Sergey Kacheev <skacheev@yandex-team.ru>
2019-06-05 23:19:44 +0700

    2.3_printable.hs

--- 2/2.3_printable.hs
+++ 2/2.3_printable.hs
@@ -7,3 +7,6 @@ instance Printable Bool where
 
 instance Printable () where
     toString () = "unit type"
+
+instance (Printable a, Printable b) => Printable (a, b) where
+    toString x = "(" ++ toString (fst x) ++ "," ++ toString (snd x) ++ ")"
